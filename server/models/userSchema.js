e39a9d379ecfb30dd6b5611c49cc61bc69e6b5bd
Harmeet Singh <96486082+Harmeet135@users.noreply.github.com>
2022-08-05 21:37:24 +0530

    Add files via upload

---
+++ server/models/userSchema.js
@@ -0,0 +1,48 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true
+//     },
+//     age: {
+//         type: Number,
+//         required: true
+//     },
+//     mobile: {
+//         type: Number,
+//         required: true
+//     },
+//     work: {
+//         type: String,
+//         required: true
+//     },
+//     desc: {
+//         type: String,
+//         required: true
+//     }
+// });
+
+// const users = new mongoose.model('users', userSchema);
+
+// module.exports = users;
+
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+    mobile: Number,
+    salary: Number,
+    work: String,
+    desc: String
+})
+
+module.exports = mongoose.model('users', userSchema);
